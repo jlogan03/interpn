4005e58741933d1bce6086d96ecb8807a2aad3a7
James Logan <jlogan03@gmail.com>
2023-11-19 13:24:54 -0500

    parametrize benchmarks over various grid sizes

--- interpn/benches/bench.rs
+++ interpn/benches/bench.rs
@@ -6,137 +6,148 @@ use interpn::{multilinear, RegularGridInterpolator};
 use randn::*;
 
 fn bench_interp(c: &mut Criterion) {
-    let mut group = c.benchmark_group("bench_interp_scanning");
-    for size in [100, 10_000, 1_000_000].iter() {
-        group.throughput(Throughput::Elements(*size as u64));
+    for gridsize in [10_usize, 100, 1000] {
+        let mut group = c.benchmark_group(format!("bench_interp_scanning_{gridsize}x{gridsize}"));
+        for size in [100, 10_000, 1_000_000].iter() {
+            group.throughput(Throughput::Elements(*size as u64));
 
-        group.bench_with_input(
-            BenchmarkId::new("10x10 Regular 2D MAXDIMS=2, Scanning", size),
-            size,
-            |b, &size| {
-                let mut rng = rng_fixed_seed();
-                let m: usize = (size as f64).sqrt() as usize;
-                let nx = 10;
-                let ny = 10;
-                let n = nx * ny;
+            group.bench_with_input(
+                BenchmarkId::new(
+                    format!("{gridsize}x{gridsize} Regular 2D MAXDIMS=2, Scanning"),
+                    size,
+                ),
+                size,
+                |b, &size| {
+                    let mut rng = rng_fixed_seed();
+                    let m: usize = (size as f64).sqrt() as usize;
+                    let nx = 10;
+                    let ny = 10;
+                    let n = nx * ny;
 
-                let x = linspace(0.0, 100.0, nx);
-                let y = linspace(0.0, 100.0, ny);
-                let z = randn::<f64>(&mut rng, n);
+                    let x = linspace(0.0, 100.0, nx);
+                    let y = linspace(0.0, 100.0, ny);
+                    let z = randn::<f64>(&mut rng, n);
 
-                // Choose observation points to be concretely inside the grid
-                // in order to avoid mixing interpolation perf with extrapolation
-                // for the points that are right on the bound.
-                let xgridobs = linspace(x[1], x[nx-2], m);
-                let ygridobs = linspace(y[1], y[ny-2], m);
-                let obsgrid = meshgrid(Vec::from([&xgridobs, &ygridobs]));
-                let xobs: Vec<f64> = obsgrid.iter().map(|xyi| xyi[0]).collect();
-                let yobs: Vec<f64> = obsgrid.iter().map(|xyi| xyi[1]).collect();
-                let obs = &[&xobs[..], &yobs[..]];
+                    // Choose observation points to be concretely inside the grid
+                    // in order to avoid mixing interpolation perf with extrapolation
+                    // for the points that are right on the bound.
+                    let xgridobs = linspace(x[1], x[nx - 2], m);
+                    let ygridobs = linspace(y[1], y[ny - 2], m);
+                    let obsgrid = meshgrid(Vec::from([&xgridobs, &ygridobs]));
+                    let xobs: Vec<f64> = obsgrid.iter().map(|xyi| xyi[0]).collect();
+                    let yobs: Vec<f64> = obsgrid.iter().map(|xyi| xyi[1]).collect();
+                    let obs = &[&xobs[..], &yobs[..]];
 
-                let mut out = vec![0.0; size];
+                    let mut out = vec![0.0; size];
 
-                b.iter(|| {
-                    black_box({
-                        // Initializing the interpolator local to where it will be used
-                        // is about a 2x speedup
-                        let dims = [nx, ny];
-                        let starts = [x[0], y[0]];
-                        let steps = [x[1] - x[0], y[1] - y[0]];
-                        let interpolator: RegularGridInterpolator<'_, _, 2> =
-                            RegularGridInterpolator::new(&dims, &starts, &steps, &z);
-                        interpolator.interp(obs, &mut out)
-                    })
-                });
-            },
-        );
+                    b.iter(|| {
+                        black_box({
+                            // Initializing the interpolator local to where it will be used
+                            // is about a 2x speedup
+                            let dims = [nx, ny];
+                            let starts = [x[0], y[0]];
+                            let steps = [x[1] - x[0], y[1] - y[0]];
+                            let interpolator: RegularGridInterpolator<'_, _, 2> =
+                                RegularGridInterpolator::new(&dims, &starts, &steps, &z);
+                            interpolator.interp(obs, &mut out)
+                        })
+                    });
+                },
+            );
 
-        group.bench_with_input(
-            BenchmarkId::new("10x10 Regular 2D MAXDIMS=8, Scanning", size),
-            size,
-            |b, &size| {
-                let mut rng = rng_fixed_seed();
-                let m: usize = (size as f64).sqrt() as usize;
-                let nx = 10;
-                let ny = 10;
-                let n = nx * ny;
+            group.bench_with_input(
+                BenchmarkId::new(
+                    format!("{gridsize}x{gridsize} Regular 2D MAXDIMS=8, Scanning"),
+                    size,
+                ),
+                size,
+                |b, &size| {
+                    let mut rng = rng_fixed_seed();
+                    let m: usize = (size as f64).sqrt() as usize;
+                    let nx = 10;
+                    let ny = 10;
+                    let n = nx * ny;
 
-                let x = linspace(0.0, 100.0, nx);
-                let y = linspace(0.0, 100.0, ny);
-                let z = randn::<f64>(&mut rng, n);
+                    let x = linspace(0.0, 100.0, nx);
+                    let y = linspace(0.0, 100.0, ny);
+                    let z = randn::<f64>(&mut rng, n);
 
-                // Choose observation points to be concretely inside the grid
-                // in order to avoid mixing interpolation perf with extrapolation
-                // for the points that are right on the bound.
-                let xgridobs = linspace(x[1], x[nx-2], m);
-                let ygridobs = linspace(y[1], y[ny-2], m);
-                let obsgrid = meshgrid(Vec::from([&xgridobs, &ygridobs]));
-                let xobs: Vec<f64> = obsgrid.iter().map(|xyi| xyi[0]).collect();
-                let yobs: Vec<f64> = obsgrid.iter().map(|xyi| xyi[1]).collect();
-                let obs = &[&xobs[..], &yobs[..]];
+                    // Choose observation points to be concretely inside the grid
+                    // in order to avoid mixing interpolation perf with extrapolation
+                    // for the points that are right on the bound.
+                    let xgridobs = linspace(x[1], x[nx - 2], m);
+                    let ygridobs = linspace(y[1], y[ny - 2], m);
+                    let obsgrid = meshgrid(Vec::from([&xgridobs, &ygridobs]));
+                    let xobs: Vec<f64> = obsgrid.iter().map(|xyi| xyi[0]).collect();
+                    let yobs: Vec<f64> = obsgrid.iter().map(|xyi| xyi[1]).collect();
+                    let obs = &[&xobs[..], &yobs[..]];
 
-                let mut out = vec![0.0; size];
+                    let mut out = vec![0.0; size];
 
-                b.iter(|| {
-                    black_box({
-                        let dims = [nx, ny];
-                        let starts = [x[0], y[0]];
-                        let steps = [x[1] - x[0], y[1] - y[0]];
-                        multilinear::regular::interpn(&dims, &starts, &steps, &z, obs, &mut out)
-                    })
-                });
-            },
-        );
+                    b.iter(|| {
+                        black_box({
+                            let dims = [nx, ny];
+                            let starts = [x[0], y[0]];
+                            let steps = [x[1] - x[0], y[1] - y[0]];
+                            multilinear::regular::interpn(&dims, &starts, &steps, &z, obs, &mut out)
+                        })
+                    });
+                },
+            );
 
-        group.bench_with_input(
-            BenchmarkId::new("10x10 Rectilinear 2D MAXDIMS=8, Scanning", size),
-            size,
-            |b, &size| {
-                let mut rng = rng_fixed_seed();
-                let m: usize = (size as f64).sqrt() as usize;
-                let nx = 10;
-                let ny = 10;
-                let n = nx * ny;
+            group.bench_with_input(
+                BenchmarkId::new(
+                    format!("{gridsize}x{gridsize} Rectilinear 2D MAXDIMS=8, Scanning"),
+                    size,
+                ),
+                size,
+                |b, &size| {
+                    let mut rng = rng_fixed_seed();
+                    let m: usize = (size as f64).sqrt() as usize;
+                    let nx = 10;
+                    let ny = 10;
+                    let n = nx * ny;
 
-                let mut x = linspace(0.0, 100.0, nx);
-                let mut y = linspace(0.0, 100.0, ny);
+                    let mut x = linspace(0.0, 100.0, nx);
+                    let mut y = linspace(0.0, 100.0, ny);
 
-                // Add noise to the grid
-                let dx = randn::<f64>(&mut rng, nx);
-                let dy = randn::<f64>(&mut rng, ny);
-                (0..nx).for_each(|i| x[i] = x[i] + (dx[i] - 0.5) / 1e3);
-                (0..ny).for_each(|i| y[i] = y[i] + (dy[i] - 0.5) / 1e3);
+                    // Add noise to the grid
+                    let dx = randn::<f64>(&mut rng, nx);
+                    let dy = randn::<f64>(&mut rng, ny);
+                    (0..nx).for_each(|i| x[i] = x[i] + (dx[i] - 0.5) / 1e3);
+                    (0..ny).for_each(|i| y[i] = y[i] + (dy[i] - 0.5) / 1e3);
 
-                // Make sure the grid is still monotonic
-                (0..nx - 1).for_each(|i| assert!(x[i + 1] > x[i]));
-                (0..ny - 1).for_each(|i| assert!(y[i + 1] > y[i]));
+                    // Make sure the grid is still monotonic
+                    (0..nx - 1).for_each(|i| assert!(x[i + 1] > x[i]));
+                    (0..ny - 1).for_each(|i| assert!(y[i + 1] > y[i]));
 
-                let z = randn::<f64>(&mut rng, n);
+                    let z = randn::<f64>(&mut rng, n);
 
-                // Choose observation points to be concretely inside the grid
-                // in order to avoid mixing interpolation perf with extrapolation
-                // for the points that are right on the bound.
-                let xgridobs = linspace(x[1], x[nx-2], m);
-                let ygridobs = linspace(y[1], y[ny-2], m);
-                let obsgrid = meshgrid(Vec::from([&xgridobs, &ygridobs]));
-                let xobs: Vec<f64> = obsgrid.iter().map(|xyi| xyi[0]).collect();
-                let yobs: Vec<f64> = obsgrid.iter().map(|xyi| xyi[1]).collect();
-                let obs = &[&xobs[..], &yobs[..]];
+                    // Choose observation points to be concretely inside the grid
+                    // in order to avoid mixing interpolation perf with extrapolation
+                    // for the points that are right on the bound.
+                    let xgridobs = linspace(x[1], x[nx - 2], m);
+                    let ygridobs = linspace(y[1], y[ny - 2], m);
+                    let obsgrid = meshgrid(Vec::from([&xgridobs, &ygridobs]));
+                    let xobs: Vec<f64> = obsgrid.iter().map(|xyi| xyi[0]).collect();
+                    let yobs: Vec<f64> = obsgrid.iter().map(|xyi| xyi[1]).collect();
+                    let obs = &[&xobs[..], &yobs[..]];
 
-                let mut out = vec![0.0; size];
+                    let mut out = vec![0.0; size];
 
-                b.iter(|| {
-                    black_box(multilinear::rectilinear::interpn(
-                        &[&x, &y],
-                        &z,
-                        obs,
-                        &mut out,
-                    ))
-                });
-            },
-        );
+                    b.iter(|| {
+                        black_box(multilinear::rectilinear::interpn(
+                            &[&x, &y],
+                            &z,
+                            obs,
+                            &mut out,
+                        ))
+                    });
+                },
+            );
+        }
+        group.finish();
     }
-    group.finish();
 }
 
 fn bench_extrap(c: &mut Criterion) {
